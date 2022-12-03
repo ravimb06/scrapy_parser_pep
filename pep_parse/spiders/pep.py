fe5d547a26207345e8fe0e7868b21758f5e5bcf9
Ali Bogatyrev <marksheyder06@gmail.com>
2022-12-03 23:45:13 +0300

    created PepSpider

---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,22 @@
+import scrapy
+import re
+
+
+class PepSpider(scrapy.Spider):
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['http://peps.python.org/']
+
+    def parse(self, response):
+        all_pep = response.css('[href^="/pep-"')
+        for pep_link in all_pep:
+            yield response.follow(pep_link, callback=self.parse_pep)
+
+
+    def parse_pep(self, response):
+        pep = response.css('h1.page-title::text').get()
+        yield {
+            'number': re.search(r'PEP (\d+)...(\w+.+)', pep).group(1),
+            'name': re.search(r'PEP (\d+)...(\w+.+)', pep).group(2),
+            'status': response.css('abbr::text').get()
+        }
